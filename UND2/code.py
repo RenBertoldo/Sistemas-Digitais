3b256da60cbd9fb042fc2e6679d1c2f55efdf234
Gabriel Caldas <gabrielhduarte@hotmail.com>
2019-05-13 07:42:36 -0300

    Pseudocódigo em java

--- UND2/code.py
+++ UND2/code.py
@@ -1,2 +1,67 @@
-Only for init
 
+List<int> heartBeatArray = new ArrayList();
+
+boolean sendInterruption = false;
+
+void main() {
+
+	
+
+	int clockDivisionTarget = "Click Galileo"/500000;
+	int clockDivisionCount = 0;
+
+
+	while(true){
+		
+		if(sendInterruption){
+
+			sendImageToPC(makeImage(heartBitArray));
+
+			heartBeatArray = new ArrayList();
+
+
+			sendInterruption = false;
+		}
+		else{
+			if(clockDivisionCount == clockDivisionTarget){
+				//Pega o valor do sensor
+				heartBeatArray.add(sensorImput.getValue());
+				clockDivisionCount=0;
+			}	
+			else{
+				clockDivisionCount++;
+			}
+		
+		}
+		
+
+	}
+
+} 
+
+void sendInfoToPC(int heartBeat){
+
+
+}
+
+void ThreadFunction(){
+
+	int clockDivisionTarget = "Click Galileo"*5;
+	int clockDivisionCount = 0;
+
+	while(true){
+
+		if(clockDivisionCount == clockDivisionTarget){
+			//Provoca a interrupção 
+			sendInterruption = true;
+			clockDivisionCount=0;
+		}	
+		else{
+			clockDivisionCount++;
+		}
+
+
+	}
+
+}
+	
